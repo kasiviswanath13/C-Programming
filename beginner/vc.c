8565f9656719fb2b1f44f20d1315800d74339d02
kasiviswanath13 <34673687+kasiviswanath13@users.noreply.github.com>
2017-12-21 21:44:18 +0530

    Create vc.c

---
+++ beginner/vc.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+  char x,y;
+  scanf("%s",x);
+  if (x==a||x==e||x==i||x==o||x==u)
+  {
+    printf("entered char is a vowel");
+    }
+    else {
+    printf("entered char is not a vowel");
+  }
+  }
