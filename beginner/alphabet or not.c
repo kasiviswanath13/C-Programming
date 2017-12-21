5890bdd27c57fa167a4b4d08c7b47d4c36c36a18
kasiviswanath13 <34673687+kasiviswanath13@users.noreply.github.com>
2017-12-21 21:49:44 +0530

    Create alphabet or not.c

---
+++ beginner/alphabet or not.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+  char s;
+  scanf("%c",&s)
+  if ((s>='a'&& s<='z')||(s>='A' && s<='Z'))
+  {
+    printf("alphabet")
+  }
+  else
+  { 
+    printf("not alphabet");
+  }
+  }
