655167c906cf7ee1d8d8bf9ba974716c37fe048b
kasiviswanath13 <34673687+kasiviswanath13@users.noreply.github.com>
2018-04-07 00:02:46 +0530

    Create sumofnaturalnumbers.c

---
+++ beginner/sumofnaturalnumbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+  int n,i,sum=0;
+  printf("enter the number till whhich u need to find");
+  scanf("%d",&n);
+  for(i=0;i<=n;i++)
+  sum=sum+i;
+  printf("the sum of natural numbers %d ",sum)
+}
