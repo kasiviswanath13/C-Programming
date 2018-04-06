ccdc5c95d95c25914e6c71b1a9a05e004bd3871f
kasiviswanath13 <34673687+kasiviswanath13@users.noreply.github.com>
2018-04-06 23:19:12 +0530

    Create largest_amoung_3numbers.c

---
+++ beginner/largest_amoung_3numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,c;
+  printf ("enter the 3 numbers");
+  scanf("%d%d%d",&a,&b,&c)
+  if (a>b)
+  printf("a is greater");
+  else if (b>c)
+  printf("b is greater");
+  else
+  printf ("c is greater");
+  
+  }
+  
