41db5b075a7c1eb3fc90cc542e265b9d049307bb
kasiviswanath13 <34673687+kasiviswanath13@users.noreply.github.com>
2018-04-06 23:49:20 +0530

    Update largestof3num.c

--- beginner/largestof3num.c
+++ beginner/largestof3num.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 void main()
 {
-int a, b,c;
-printf ("enter the numbers");
-scanf("%d%d%d",&a,&b,&c);
-if(a>b)
-{
-  printf("a is greater");
- }
- else if (b>c)
- {
- printf("c is greater");
- }
- else 
- {
- printf("c is greater");
- }
- }
+  int a, b,c;
+  printf ("enter the numbers");
+  scanf("%d%d%d",&a,&b,&c);
+  if(a>b)
+  {
+    printf("a is greater");
+  }
+    else if (b>c)
+   {
+    printf("c is greater");
+   }
+   else 
+   {
+    printf("c is greater");
+   }
+}
  
