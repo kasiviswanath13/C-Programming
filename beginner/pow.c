e317ff082140218cb00dab840ddb642db2c7490b
kasiviswanath13 <34673687+kasiviswanath13@users.noreply.github.com>
2018-04-07 00:25:22 +0530

    Update pow.c

--- beginner/pow.c
+++ beginner/pow.c
@@ -1,1 +1,12 @@
 
+#include<stdio.h>
+void main()
+{
+  double n,i,k;
+  printf("enter the base");
+  scanf("%lf",&n);
+  printf("enter the exponent")
+    scanf("%lf",&i);
+  k=pow(n,i);
+  printf("the ans is %lf",k);
+}
